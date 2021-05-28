f2068d498b99051b46793336bb6082c21c5a8ce7
predeezRav <prat0025@edu.ucl.dk>
2021-05-28 21:38:48 +0200

    Submit

---
+++ api-eksamen/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '55f20c278f4eb18827e945eb53ea5951'),
+    },
+  },
+});
---
+++ api-eksamen/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '38cf7472-fac7-4bb2-ad0b-0e96abeb858b'
+};
